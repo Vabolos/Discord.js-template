5f1bdb0f21cfe909318b9256b17faaddccdd82a3
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-10-31 16:06:31 +0100

    Update upTime.js

--- commands/upTime.js
+++ commands/upTime.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const humanizeDuration = require("humanize-duration");
+const humanizeDuration = require('humanize-duration');
 
 module.exports = {
     data: new SlashCommandBuilder()
